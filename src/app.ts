3387ef0e97d9416637ce3d51eabe3a528772525d
iuculano <82924525+iuculano@users.noreply.github.com>
2024-05-06 03:08:29 +0000

    Forget to commit this so it actually runs code

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import { Rom } from './chip-8/rom';
 import { Disassembler } from './chip-8/disassembler';
 import { OpcodeDecoder } from './chip-8/opcode';
+import { Processor } from './chip-8/processor';
 
 const rom = new Rom();
 rom.fromFile('./roms/IBM Logo.ch8');
 
-for (const opcode of rom.generateOpcodes()) {
-  const instruction = OpcodeDecoder.decode(opcode);
-  const output = Disassembler.disassemble(instruction);
-  console.log(output);
+const cpu = new Processor();
+cpu.mapRom(rom);
+
+for (let i = 0; i < 1000; i++) {
+  cpu.step(true);
 }
+
+//for (const opcode of rom.generateOpcodes()) {
+//  const instruction = OpcodeDecoder.decode(opcode);
+//  const output = Disassembler.disassemble(instruction);
+//  console.log(output);
+//}
